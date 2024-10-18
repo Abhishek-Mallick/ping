116b8fd4cbd03380665c9f1fff02fc47ed87346e
Abhishek Mallick <mallickabhishek97@gmail.com>
2024-10-19 05:25:00 +0530

    server: added middleware

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -1,23 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 
 export async function initServer() {
     const app = express();
+
+    app.use(bodyParser.json()); // middleware
+
     const graphqlServer = new ApolloServer({
         typeDefs: `
             type Query {
                 sayHello: String
-            }
-            type Mutation {
-                
             }
         `,
         resolvers: {
             Query: {
                 sayHello: () => 'Hello from GraphQL server',
             },
-            Mutation: {},
         },
       });
     
